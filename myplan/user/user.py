191b1719cc606990fc507adc2b363abafa234e95
Cezar Boicu <cezar.boicu@gmail.com>
2013-12-11 13:15:30 +0200

    added file for common declarations and added some fields to the user class

--- myplan/user/user.py
+++ myplan/user/user.py
@@ -1,5 +1,16 @@
+#The class User holds any user which is creating an account
+#The user has a certain amount of privileges which can grant him certain rights / features
 
+from django.db import models
 
 class User:
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    date_joined = models.DateTimeField()
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    sex = 
     def __init__(self):
         pass
+
