07a949074067aa225bfdb778bf835f741fb6244f
Cezar Boicu <cezar.boicu@gmail.com>
2014-02-11 19:54:21 +0200

    added template and is working

--- myplan/planner/views.py
+++ myplan/planner/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-
+from django.template import RequestContext, loader
 
 def index_planner(request):
-    return HttpResponse('Welcome Please log in or sign up')
+    template = loader.get_template('planner/index.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
